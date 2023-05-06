6a5879ffffff6cbc63c5a75e285a45804a3858e7
IXIOverlorDIXI <andrii.dolhyi@nure.ua>
2023-05-07 00:23:52 +0300

    Model fix

--- Application/DTOs/RelativePositionMeteringDto.cs
+++ Application/DTOs/RelativePositionMeteringDto.cs
@@ -1,41 +1,29 @@
 ﻿using System;
-using Newtonsoft.Json;
 
 namespace Application.DTOs
 {
     public class RelativePositionMeteringDto
     {
-        [JsonProperty("Id")]
         public Guid Id { get; set; }
-        
-        [JsonProperty("LeftDistance")]
+
         public double LeftDistance { get; set; }
-        
-        [JsonProperty("RightDistance")]
+
         public double RightDistance { get; set; }
-        
-        [JsonProperty("FrontDistance")]
+
         public double FrontDistance { get; set; }
-        
-        [JsonProperty("BackDistance")]
+
         public double BackDistance { get; set; }
-        
-        [JsonProperty("RotationAngle")]
+
         public double RotationAngle { get; set; }
-        
-        [JsonProperty("TiltAngle")]
+
         public double TiltAngle { get; set; }
-        
-        [JsonProperty("HeightHeadAboveWater")]
+
         public double HeightHeadAboveWater { get; set; }
-        
-        [JsonProperty("HeightTailAboveWater")]
+
         public double HeightTailAboveWater { get; set; }
-        
-        [JsonProperty("BerthId")]
+
         public Guid BerthId { get; set; }
-        
-        [JsonProperty("MeteringDate")]
+
         public DateTime MeteringDate { get; set; }
     }
 }
